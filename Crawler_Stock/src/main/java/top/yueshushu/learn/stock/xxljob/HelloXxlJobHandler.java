6a36f4553730e5d7032babc875eedf7658dd9534
yjl <qwe@163.com>
2021-11-23 19:50:12 +0800

    feat: 引入 xxl-job 配置信息

---
+++ Crawler_Stock/src/main/java/top/yueshushu/learn/stock/xxljob/HelloXxlJobHandler.java
@@ -0,0 +1,28 @@
+package top.yueshushu.learn.stock.xxljob;
+
+import cn.hutool.core.date.DateUtil;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName:HelloXxlJob
+ * @Description TODO
+ * @Author zk_yjl
+ * @Date 2021/11/23 19:06
+ * @Version 1.0
+ * @Since 1.0
+ **/
+/**
+ 这样调用是不对的。
+ */
+@Component("helloXxlJobHandler")
+@Log4j2
+public class HelloXxlJobHandler extends IJobHandler {
+
+   @Override
+    public void execute() throws Exception {
+        log.info("Handler: >>>>开始执行策略"+ DateUtil.now());
+    }
+}
